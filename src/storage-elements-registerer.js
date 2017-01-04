3170ad71df7d7315b4064ec76e6d4c9387171477
Keiichiro Ui <keiichiro.ui@gmail.com>
2017-01-04 15:28:07 +0900

    Fix to more exact comment

--- src/storage-elements-registerer.js
+++ src/storage-elements-registerer.js
@@ -1,6 +1,6 @@
 /* global chrome */
 
-import StorageForm from "./storage-form";
+import StorageFormElement from "./storage-form";
 import * as ah from "./area-handler";
 
 // Register area handlers
@@ -15,10 +15,11 @@ if (chrome && chrome.storage) {
     ah.registerHandler("chrome-sync", new ah.ChromeStorageAreaHandler(chrome.storage.sync));
 }
 
-// Custom Element v1 seems not to working right on Google Chrome 55
-// customElements.define(name, ce, { extends: ex });
+// Custom Element v1 seems not to works right to extend <form> in Google Chrome 55
+// customElements.define("storage-form", StorageFormElement, { extends: "form" });
+// window.StorageFormElement = StorageFormElement;
 
 // Custom Element v0
-// $FlowFixMe Avoid to affect code of `storage-form.js` by Custom Element v0
-Object.defineProperty(StorageForm, "extends", { get: () => "form" });
-document.registerElement("storage-form", StorageForm);
+// $FlowFixMe Force define to avoid to affect code of `storage-form.js` by Custom Element v0
+Object.defineProperty(StorageFormElement, "extends", { get: () => "form" });
+document.registerElement("storage-form", StorageFormElement);
